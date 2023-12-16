8012512ec8bcd261ec4af80b8bcc551f2ed29a0b
Emretester <emreyeni2019@gmail.com>
2023-12-16 11:06:29 +0300

    day 18 completed

--- src/day01_JavaIntro/Test.java
+++ src/day01_JavaIntro/Test.java
@@ -10,21 +10,9 @@ public class Test {
 
         StringUtility.printEachChar(str);
 
-        for (int j = 0; j < 10; j++) {
-
-            for (int i = 0; i < 10; i++) {
-
-                System.out.print("*");
-
-            }
-
-            System.out.println();
-
-        }
-
+        String line = "";
 
 
     }
-
 
 }
