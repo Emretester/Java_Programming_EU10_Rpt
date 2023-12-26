f02432b4697451c851f7c224ff136fffd71896f7
Emretester <emreyeni2019@gmail.com>
2023-12-26 20:50:41 +0300

    day 21 completed

--- src/day01_JavaIntro/Test.java
+++ src/day01_JavaIntro/Test.java
@@ -2,6 +2,8 @@ package day01_JavaIntro;
 
 import utilities.StringUtility;
 
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -12,6 +14,16 @@ public class Test {
 
         String line = "";
 
+        String str1 = "ab";
+        System.out.println(str1.toString());
+
+        int a = 1;
+
+        System.out.println(a);
+
+        System.out.println(3);
+
+        System.out.println(Arrays.toString("emre".toCharArray()));
 
     }
 
